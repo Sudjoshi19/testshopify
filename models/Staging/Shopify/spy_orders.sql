e3581a59dd61012f1447d011d176d27270c642aa
sudhanshu.joshi@sarasanalytics.com <sudhanshu.joshi@sarasanalytics.com>
2023-06-28 09:29:14 +0000

    Commit

--- models/Staging/Shopify/spy_orders.sql
+++ models/Staging/Shopify/spy_orders.sql
@@ -18,7 +18,7 @@ SELECT coalesce(MAX(_daton_batch_runtime) - 2592000000,0) FROM {{ this }}
 
 
 {% set table_name_query %}
-{{set_table_name('%weezie%shopify%orders%')}}    
+{{set_table_name('%test%rawdata%sud%')}}    
 {% endset %}  
 
 {% set results = run_query(table_name_query) %}
